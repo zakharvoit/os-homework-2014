b7c2aa3cc5e3ae9bace62a229e4e088a9aec1e41
Zakhar Voit <zakharvoit@gmail.com>
2015-03-10 19:01:07 +0300

    Add the cat utility

---
+++ cat/cat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <helpers.h>
+
+#define BUFFER_SIZE 4096
+
+int main()
+{
+    char buffer[BUFFER_SIZE];
+    int count;
+
+    while ((count = read_(STDIN_FILENO, buffer, BUFFER_SIZE)) != 0) {
+        if (count < 0) {
+            goto ERROR;
+        }
+        if (write_(STDOUT_FILENO, buffer, count) < 0) {
+            goto ERROR;
+        }
+    }
+
+    return EXIT_SUCCESS;
+
+ERROR:
+    perror("Error while processing files");
+
+    return EXIT_FAILURE;
+}
---
+++ cat/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+for i in `seq 1 100` ; do
+   echo `head -n 100 /dev/urandom`  >.buffer.txt
+   ./cat <.buffer.txt >.actual.txt
+   cat .buffer.txt >.expected.txt
+   cmp .expected.txt .actual.txt
+   if [[ $? != 0 ]] ; then
+       echo "Test $i failed see .buffer.txt";
+       rm -f .expected.txt .actual.txt
+       exit 1
+   fi
+   echo "Test $i ok"
+done
+
+rm -f .expected.txt .actual.txt .buffer.txt
